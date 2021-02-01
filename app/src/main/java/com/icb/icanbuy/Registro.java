a7640fcbb7a06ec37802dcbee1cda216f9ea2582
vvfidelitas <viviana.villalobos5@gmail.com>
2021-01-31 20:30:11 -0600

    SMTP

--- app/src/main/java/com/icb/icanbuy/Registro.java
+++ app/src/main/java/com/icb/icanbuy/Registro.java
@@ -3,12 +3,38 @@ package com.icb.icanbuy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.net.Uri;
 
 public class Registro extends AppCompatActivity {
-
+    private EditText txtCorreo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+
+        txtCorreo = findViewById(R.id.editTextTextEmailAddress);
+        String edttxt = txtCorreo.getText().toString();
+
+        final Button send = (Button) this.findViewById(R.id.button3);
+        send.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                try {
+                    GMailSender sender = new GMailSender(edttxt, "password");
+                    sender.sendMail("This is Subject",
+                            "This is Body",
+                            "user@gmail.com",
+                            edttxt);
+                } catch (Exception e) {
+                    Log.e("SendMail", e.getMessage(), e);
+                }
+
+            }
+        });
     }
 }
