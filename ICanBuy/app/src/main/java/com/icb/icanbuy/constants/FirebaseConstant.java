d9a85dc0ea608338c0f6255c3066a3f0d982f61e
erickdadice <erickddcerdas@gmail.com>
2021-02-16 23:35:05 -0600

    Api airtable, url y key

---
+++ ICanBuy/app/src/main/java/com/icb/icanbuy/constants/FirebaseConstant.java
@@ -0,0 +1,8 @@
+
+package com.icb.icanbuy.constants;
+
+public final class FirebaseConstant {
+    public final static String APPLICATION_TAG= "ICanBuy";
+    public final static String INITIALIZE_PROPERTIES_ERROR_MESSAGE= "Error at initializeProperties()";
+}
+
---
+++ ICanBuy/app/src/main/java/com/icb/icanbuy/services/DALService.java
@@ -0,0 +1,4 @@
+package com.icb.icanbuy.services;
+
+public class DALService {
+}
